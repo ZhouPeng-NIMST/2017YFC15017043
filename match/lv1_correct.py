d3cde474c60c3ec2536be6ea24e416ae09555ed5
zzllg <296160709@qq.com>
2019-07-25 10:52:43 +0800

    Add files via upload

--- match/lv1_correct.py
+++ match/lv1_correct.py
@@ -1,7 +1,11 @@
+
 # coding: utf-8
+
+
 import numpy as np
 import pandas as pd
 import os 
+
 
 def get_correct(data):
     '''
@@ -11,7 +15,7 @@ def get_correct(data):
     idx_mon_1 = {'2018-01':-1,'2018-02':-1,'2018-03':-1,'2018-04':-1,'2018-05':-1,
                  '2018-06':-1,'2018-07':-1,'2018-08':-1,'2018-09':-1,'2018-10':-1}
 
-    wb_mon = {'2018-01':[-1,-1,-1],'2018-02':[-1,-1,-1],'2018-03':[-1,-1,-1],'2018-04':[-1,-1,-1],'2018-05':[-1,-1,-1],
+    ewb_mon = {'2018-01':[-1,-1,-1],'2018-02':[-1,-1,-1],'2018-03':[-1,-1,-1],'2018-04':[-1,-1,-1],'2018-05':[-1,-1,-1],
               '2018-06':[-1,-1,-1],'2018-07':[-1,-1,-1],'2018-08':[-1,-1,-1],'2018-09':[-1,-1,-1],'2018-10':[-1,-1,-1]}
 
     # record ids of every month
@@ -42,23 +46,45 @@ def get_correct(data):
                 e.append(e_j)
                 w.append(f[0])
                 b.append(f[1])
-            wb_mon[o][0] = e
-            wb_mon[o][1] = w
-            wb_mon[o][2] = b
+            ewb_mon[o][0] = e
+            ewb_mon[o][1] = w
+            ewb_mon[o][2] = b
             last_mon = o
         elif last_mon != '':
-            wb_mon[o][0] = wb_mon[last_mon][0]
-            wb_mon[o][1] = wb_mon[last_mon][1]
-            wb_mon[o][2] = wb_mon[last_mon][2]
+            ewb_mon[o][0] = ewb_mon[last_mon][0]
+            ewb_mon[o][1] = ewb_mon[last_mon][1]
+            ewb_mon[o][2] = ewb_mon[last_mon][2]
         elif last_mon == '':
-            wb_mon[o][0] = 0
-            wb_mon[o][1] = 0
-            wb_mon[o][2] = 0
-    return wb_mon
+            ewb_mon[o][0] = 0
+            ewb_mon[o][1] = 0
+            ewb_mon[o][2] = 0
+    return ewb_mon
+
+def correct(data, ewb_mon):
+    '''
+    基于得到的wb_mon对观测数据进行偏差订正
+    return: datafram of lv1 corrected
+    '''
+    out = []
+    for i, date in enumerate(data.iloc[:,0]):
+        data_thistime = []
+        mon = date[:7]
+        if mon in ewb_mon:
+            data_thistime.append(date)
+            e = ewb_mon[mon][0]
+            w = ewb_mon[mon][1]
+            b = ewb_mon[mon][2]
+            for j in range(1,18):
+                data_thistime.append(np.round(w[j - 1]*(data.iloc[i,j] + e[j - 1]) + b[j - 1], 2))
+            out.append(data_thistime)
+    return pd.DataFrame(out,columns= None)
 
 if __name__ == '__main__':
     stationNum = '54511'
-    path_in = r'F:\data\beijing2018\LV\LV1\lv1_mono_matched'
-    data = pd.read_csv(os.path.join(path_in,stationNum + '_matched.csv'), index_col= None, header= None)
-    wb_mon = get_correct(data)
+    path_in = r'F:/data/beijing2018/LV/LV1/lv1_mono_matched/'+ stationNum + '_matched.csv'
+    pathout = 'F:/data/beijing2018/LV/LV1/lv1_corrected/'+ stationNum + '_corrected.csv'
+    data = pd.read_csv(path_in, index_col= None, header= None)
+    ewb_mon = get_correct(data)
+    lv1_corrected = correct(data, ewb_mon)
+    lv1_corrected.to_csv(pathout, index= None, header= None)
 
